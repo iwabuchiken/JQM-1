1d08db479fdc72c1eee24f780d7d35046ed65d66
iwabuchiken <iwabuchi.k.2010@gmail.com>
2013-11-01 11:09:23 +0900

    D-4(m)
    v-1.59
    ---
    1. log => Button "Show log" ==> Done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -110,6 +110,8 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params[:article])
 
+
+
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
