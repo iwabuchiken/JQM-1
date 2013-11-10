c1ba60a1f0e6c093ee91156a4f078ffc1a7f65a7
iwabuchiken <iwabuchi.k.2010@gmail.com>
2013-11-10 23:52:05 +0900

    D-4
    v-1.7.3.2
    ---
    1. "Genres" => Alert shown using Ajax

--- app/assets/javascripts/keywords.js
+++ app/assets/javascripts/keywords.js
@@ -1,5 +1,25 @@
 function show_genre_list() {
 	
-	alert("Show genre list");
+//	alert("Show genre list");
+	$.ajax({
+		
+	    url: "/nr4/keywords/show_genre_list",
+	    type: "GET",
+	    timeout: 10000
+	    
+	}).done(function(data, status, xhr) {
+		
+//	    $("#js").html(data);
+//	    $("#js").append(data);
+	    $("#genre_list").prepend(data);
+//	    $("#js").append("<br/>");
+	    
+	    
+	}).fail(function(xhr, status, error) {
+		
+	    $("#genre_list").append("xhr.status = " + xhr.status + "<br>");          // 例: 404
+	    
+	});
+
 	
 }
