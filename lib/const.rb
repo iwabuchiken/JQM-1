94c3deb6be21c3e40251133b9f380a9a123dea90
iwabuchiken <iwabuchi.k.2010@gmail.com>
2013-12-16 19:43:18 +0900

    D-4
    v-4.1.3.4
    ---
    1. csv files => Created & downloaded

---
+++ lib/const.rb
@@ -0,0 +1,13 @@
+module Const
+    
+    BACKUP_PATH = "doc/backup"
+    
+    MODELS_PATH = "app/models"
+    
+    LOG_PATH    = "doc/mylog"
+    
+    LOG_FILE_NAME = "log.log"
+    
+    # BACKUP_URL    = "http://benfranklin.chips.jp/rails_apps/nr4/cakephp-2.3.10/words/add"
+    
+end
