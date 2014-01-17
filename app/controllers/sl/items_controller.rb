31474fb3bb2ac26a71a3784e1bc8f3bdea0a9395
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-01-17 10:39:12 +0900

    D-3
    v-1.1.0
    ---
    1. Move the method "_new_data_from_device"  => Independent method
    	==> Done

--- app/controllers/sl/items_controller.rb
+++ app/controllers/sl/items_controller.rb
@@ -151,11 +151,14 @@ class Sl::ItemsController < ApplicationController
     end#delete_all
 
 private
+  #=================================
+  # => _new_data_from_device
+  # => @return false => param "passwd_XX" --> nill
+  # =>          true => Operation done
+  #=================================
   def _new_data_from_device
         
-        if !params['passwd_sl']
-        #if !params['passwd']
-        # if !params['passwd_sl']
+        if !params[Const::SL::PASSWD_SL_Key]
             
             #debug
             write_log(
@@ -164,9 +167,6 @@ private
                   # __FILE__,
                   __FILE__.split("/")[-1],
                   __LINE__.to_s)
-            
-            
-        # if !params['password_sl']
             
             return false
             
@@ -180,7 +180,7 @@ private
                   # __FILE__,
                   __FILE__.split("/")[-1],
                   __LINE__.to_s)
-            
+=begin            
             #REF app name // http://stackoverflow.com/questions/3539148/how-do-i-access-the-name-of-the-rails-3-application-object Rails.application.class.to_s.split("::").first
             #REF method name // http://stackoverflow.com/questions/6426598/how-to-get-current-method-in-rails-3 answered Jun 21 '11 at 15:53
             #REF cont name // http://stackoverflow.com/questions/3757491/can-i-get-the-name-of-the-current-controller-in-the-view answered Sep 21 '10 at 5:27
@@ -192,75 +192,21 @@ private
                   # __FILE__,
                   __FILE__.split("/")[-1],
                   __LINE__.to_s)
-    
-            #if params['item']
-            #if params['item']
-            
-                # msg = "#{params['item']}(#{params['item']['name']})"
-                # msg = params['item']
-                
-                item = Item.new()
-                #item = Item.new
-                
-                #item.name = params['item']['name']
-                #item.id = params['item_store_id']
-                item.mobile_id  = params['item_mobile_id']
-                
-                item.name       = params['item_name']
-                item.store_id   = params['item_store_id']
-                
-                item.price      = params['item_price']
-                item.genre_id   = params['item_genre_id']
-                
-                item.yomi       = params['item_yomi']
-                
-                item.mobile_created_at  = params['item_mobile_created_at']
-                item.mobile_updated_at  = params['item_mobile_updated_at']
-                item.mobile_posted_at   = params['item_mobile_posted_at']
-                
-=begin
-            "item_store_id",  "item_name",
-            "item_price",   "item_genre_id",
-            "item_yomi",    "item_mobile_id",
-            
-            t.integer  "store_id"
-            t.string   "name"
-            t.integer  "price"
-            t.integer  "genre_id"
-            t.string   "yomi"
-            t.integer  "mobile_id"
-            t.datetime "created_at", :null => false
-            t.datetime "updated_at", :null => false
-  
 =end
-                if item.save
-                    
-                    msg = "New item saved => #{item.class.to_s}"
-                    #msg = "New item saved => #{params['item']}"
-                    
-                else
-                    
-                    msg = "Saving new item => Failed"
-                    
-                end
+            msg = ""
+            
+            if params[Const::SL::PASSWD_SL_Key] \
+                        == Const::SL::PASSWD_SL_NewItem
                 
-              
-            #else
-=begin
-                msg = "params['item'] => null"
-                
-                #debug
-                write_log(
-                      Const::SL::LOG_PATH_SL,
-                      msg,
-                      # __FILE__,
-                      __FILE__.split("/")[-1],
-                      __LINE__.to_s)
-                
-                return false
-=end
-            #end#if params['item']
+                msg = _new_data_from_device_NewItem
     
+            elsif params[Const::SL::PASSWD_SL_Key] \
+                        == Const::SL::PASSWD_SL_PurHist#if params['passwd_sl'] == Const::SL::PASSWD_SL_NEW
+                
+                msg = _new_data_from_device_PurHist
+                
+            end#if params['passwd_sl'] == Const::SL::PASSWD_SL_NEW
+            
               #debug
             write_log(
                   Const::SL::LOG_PATH_SL,
@@ -268,20 +214,77 @@ private
                   # __FILE__,
                   __FILE__.split("/")[-1],
                   __LINE__.to_s)
-=begin
-            respond_to do |format|
-                
-                format.json {render json: item}
-                
-            end
-            # render :text => msg
-            # render :text => "DONE"
-=end
+                  
             return true
             
         end#if !params['passwd_sl']
         
   end#def _new__1_data_from_device
 
+    def _new_data_from_device_PurHist
+        
+        item = Item.new()
+        #item = Item.new
+        
+        item.mobile_id  = params['item_mobile_id']
+        
+        item.name       = params['item_name']
+        item.store_id   = params['item_store_id']
+        
+        item.price      = params['item_price']
+        item.genre_id   = params['item_genre_id']
+        
+        item.yomi       = params['item_yomi']
+        
+        item.mobile_created_at  = params['item_mobile_created_at']
+        item.mobile_updated_at  = params['item_mobile_updated_at']
+        item.mobile_posted_at   = params['item_mobile_posted_at']
+        
+        if item.save
+            
+            msg = "New item saved => #{item.class.to_s}"
+            
+        else
+            
+            msg = "Saving new item => Failed"
+            
+        end
+
+        return msg
+        
+    end#def _new_data_from_device_PurHist
+    
+    def _new_data_from_device_NewItem
+
+        item = Item.new()
+        #item = Item.new
+        
+        item.mobile_id  = params['item_mobile_id']
+        
+        item.name       = params['item_name']
+        item.store_id   = params['item_store_id']
+        
+        item.price      = params['item_price']
+        item.genre_id   = params['item_genre_id']
+        
+        item.yomi       = params['item_yomi']
+        
+        item.mobile_created_at  = params['item_mobile_created_at']
+        item.mobile_updated_at  = params['item_mobile_updated_at']
+        item.mobile_posted_at   = params['item_mobile_posted_at']
+        
+        if item.save
+            
+            msg = "New item saved => #{item.class.to_s}"
+            
+        else
+            
+            msg = "Saving new item => Failed"
+            
+        end
+
+        return msg
+        
+    end#def _new_data_from_device_NewItem
 
 end
